bc633b83c19556623d5a2037280b21b7503f794f
Mr.Z <zrd0921@qq.com>
2020-08-01 18:27:02 +0800

    add demo

---
+++ demo/source.ts
@@ -0,0 +1,11 @@
+class Animal {
+  public name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  sayHi(): string {
+    return `My name is ${this.name}`;
+  }
+}
+
+let a: Animal = new Animal('Jack');
---
+++ demo/source.vue
@@ -0,0 +1,34 @@
+<template>
+	<div v-if="show" @click="handleClick">
+		<span>Hello world</span>
+	</div>
+</template>
+
+<script>
+import { Button } from '@vsc/cc';
+
+export default {
+	name: 'vsc-vue',
+	components: {
+        'vsc-button': Button
+    },
+	props: {},
+	data() {
+		return {
+            show: false
+        };
+	},
+	created() {	},
+	methods: { 
+        handleClick(e = {}) {
+            console.log(`click`);
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+span {
+    font-size: 16px;
+}
+</style>
