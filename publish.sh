0bf46a37e3ba85e654f375d50e5fcff73d4cb34a
Mr.Z <zrd0921@qq.com>
2026-02-03 11:01:17 +0800

    [build] v1.0.9

---
+++ publish.sh
@@ -0,0 +1,35 @@
+set -e
+
+if [[ -z $1 ]]; then
+	echo "Enter new version: "
+	read VERSION
+else
+	VERSION=$1
+fi
+
+read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Releasing v$VERSION ..."
+
+	# build 
+	VERSION=$VERSION pnpm run compile
+
+	# version
+	npm version $VERSION --no-git-tag-version 
+
+	# commit
+	git add -A
+	git commit -m "[build] v$VERSION"
+
+	# package
+	npx @vscode/vsce package
+
+	# publish
+	# npx @vscode/vsce login deot
+	npx @vscode/vsce publish
+
+	# push
+	# git push origin refs/tags/v$VERSION
+	git push origin master
+fi
